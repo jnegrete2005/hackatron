ea0661528b3bbd9e9a2442921ebeb607c9108df0
Joaquin Negrete <joaquin.negrete@gmail.com>
2025-06-21 16:30:52 +0200

    Base player class and init file for players module

---
+++ src/players/base.py
@@ -0,0 +1,19 @@
+from src.GameState import GameState
+
+
+class BaseBot:
+    def __init__(self):
+        pass
+
+    def get_move(self, game_state: GameState) -> int:
+        """
+        This method should be implemented by subclasses to return the next move.
+        :param game_state: The current state of the game.
+        :type game_state: Board
+
+        :raise NotImplementedError: If the method is not implemented by a subclass.
+
+        :return: The next move to be made.
+        :rtype: int
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
