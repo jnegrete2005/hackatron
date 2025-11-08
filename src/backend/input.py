3115f4c4612cd3f1a4033283c9e8c2860ad91881
Joaquin Negrete <joaquin.negrete@gmail.com>
2025-11-08 12:54:52 +0100

    Use docker bots to get input

--- src/backend/input.py
+++ src/backend/input.py
@@ -1,20 +1,32 @@
-from src.backend.GameState import GameState
-from src.backend.player import Player
-
-from src.bots.player_1.bot import Bot as PlayerOneBot
-from src.bots.player_2.bot import Bot as PlayerTwoBot
+import asyncio
 
 
-async def get_input(player: Player, game: GameState) -> int:
+async def get_input(game: str, bot: asyncio.subprocess.Process) -> int:
     """
     Get the input from the appropriate bot based on the player number.
 
-    :param player: The player to get the input for
-    :type player: Player
-    :param game: The current game state
-    :type game: GameState
+    :param game: The current game state with player number included as a JSON string.
+    :type game: str
+    :param bot: The subprocess handle for the bot
+    :type bot: asyncio.subprocess.Process
     """
-    if player.number == 1:
-        return PlayerOneBot(player).get_move(game)
-    else:
-        return PlayerTwoBot(player).get_move(game)
+    if bot is None or bot.stdin is None or bot.stdout is None:
+        raise ValueError("Bot subprocess is None")
+    try:
+        bot.stdin.write((game + "\n").encode("utf-8"))
+        await bot.stdin.drain()
+
+        output = await bot.stdout.readline()
+
+        if not output:
+            print("No output received from bot")
+            return -1
+
+        print(f"Received output from bot: {output.strip().decode('utf-8')}")
+        return int(output.strip().decode('utf-8'))
+    except Exception as e:
+        print(f"Error getting input from bot: {e}")
+        return -1
+    finally:
+        print("Finished get_input function")
+        print()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import asyncio
-
+import json
 import pygame
 
+from src.backend.bots import launch_bots, get_bot_args, terminate_bots
+from src.backend.consts import PLAYER_1, PLAYER_2
 from src.backend.GameState import GameState
 from src.backend.input import get_input
 from src.backend.player import Player
@@ -9,22 +11,32 @@ from src.backend.player import Player
 from src.frontend.Frontend import Frontend
 
 
-async def get_moves(game: GameState) -> tuple[tuple[int, int], tuple[int, int]]:
+async def get_moves(
+    game: GameState,
+    bot_1: asyncio.subprocess.Process,
+    bot_2: asyncio.subprocess.Process
+) -> tuple[tuple[int, int], tuple[int, int]]:
     """
     Get moves from both players concurrently.
 
     :param game: The current game state.
     :type game: GameState
+    :param bot_1: The subprocess handle for bot 1.
+    :type bot_1: asyncio.subprocess.Process
+    :param bot_2: The subprocess handle for bot 2.
+    :type bot_2: asyncio.subprocess.Process
 
     :return: A tuple containing the moves of player 1 and player 2.
     :rtype: tuple[tuple[int, int], tuple[int, int]]
     """
-    player_1 = game.player_1
-    player_2 = game.player_2
+    player_1, player_2 = game.player_1, game.player_2
+
+    state_for_p1 = game.serialize_for_player(PLAYER_1)
+    state_for_p2 = game.serialize_for_player(PLAYER_2)
 
     move_1, move_2 = await asyncio.gather(
-        get_input(player_1, game),
-        get_input(player_2, game)
+        get_input(json.dumps(state_for_p1), bot_1),
+        get_input(json.dumps(state_for_p2), bot_2)
     )
 
     if not Player.is_valid_move(move_1) or player_1.player_suicided(move_1):
@@ -35,7 +47,7 @@ async def get_moves(game: GameState) -> tuple[tuple[int, int], tuple[int, int]]:
     return move_1, move_2
 
 
-async def play(game: GameState, frontend: Frontend):
+async def play(game: GameState, frontend: Frontend, bot_1: asyncio.subprocess.Process, bot_2: asyncio.subprocess.Process):
     """
     Play the game until it's over.
 
@@ -43,9 +55,13 @@ async def play(game: GameState, frontend: Frontend):
     :type game: GameState
     :param frontend: The frontend to draw the game board.
     :type frontend: Frontend
+    :param bot_1: The subprocess handle for bot 1.
+    :type bot_1: asyncio.subprocess.Process
+    :param bot_2: The subprocess handle for bot 2.
+    :type bot_2: asyncio.subprocess.Process
     """
     while not game.game_over:
-        move_1, move_2 = await get_moves(game)
+        move_1, move_2 = await get_moves(game, bot_1, bot_2)
         game.tick(move_1, move_2)
         frontend.draw_game_board()
         await wait_for_keypress()
@@ -67,12 +83,24 @@ async def main():
     """
     Initialize the game and frontend, then start playing.
     """
+    bot_1_image, bot_2_image = get_bot_args()
+    bot_1, bot_2 = await launch_bots(bot_1_image, bot_2_image)
+
+    if bot_1 is None or bot_2 is None:
+        print("Failed to launch bots. Exiting.")
+        await terminate_bots(bot_1, bot_2)
+        return
+
     game = GameState(16)
     frontend = Frontend(game, 30)
-
     frontend.draw_game_board()
-    await play(game, frontend)
 
+    try:
+        await play(game, frontend, bot_1, bot_2)
+    except Exception as e:
+        print(f"Error occurred while playing: {e}")
+    finally:
+        await terminate_bots(bot_1, bot_2)
 
 if __name__ == "__main__":
     asyncio.run(main())
